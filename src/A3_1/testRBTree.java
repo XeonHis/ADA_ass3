38127d6e44c9744c5f5c6be286e27e570f8de1f6
mr.x <yingheting@gmail.com>
2019-11-27 21:18:05 +0800

    A3_1 init

---
+++ src/A3_1/testRBTree.java
@@ -0,0 +1,22 @@
+package A3_1;
+
+/**
+ * @author paulalan
+ * @create 2019/11/27 15:26
+ */
+public class testRBTree
+{
+	public static void main(String[] args)
+	{
+		RBTree rbTree = new RBTree();
+//		int[] testArray = {10, 20, -10, 15, 17, 40, 50, 60};
+		int[] testArray = {72,0,61,80,77,82,82,95,30,40,0};
+		for (int value : testArray)
+		{
+			rbTree.insert(value);
+		}
+		rbTree.inOrder();
+		rbTree.miniGap();
+		System.out.println(rbTree.getMinimum());
+	}
+}
---
+++ src/test.java
@@ -0,0 +1,21 @@
+/**
+ * @author paulalan
+ * @create 2019/11/27 21:10
+ */
+public class test
+{
+	public static void main(String[] args)
+	{
+		System.out.println(("72 \n" +
+				"0 \n" +
+				"61 \n" +
+				"80 \n" +
+				"77 \n" +
+				"82 \n" +
+				"82 \n" +
+				"95 \n" +
+				"30 \n" +
+				"40 \n" +
+				"0").replace(" \n", ","));
+	}
+}
